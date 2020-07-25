124a86576a31b4ca599d5ac30687c31386e0a031
Aline-Acs <aline_capoani@hotmail.com>
2020-07-25 10:41:10 -0300

    no message

--- src/routes.js
+++ src/routes.js
@@ -8,12 +8,16 @@ routes.get('/notes', (req, res) =>
   res.json({
     notes: [
       {
-        title: 'notes',
-        content: 'teste',
+        title: 'nota 1',
+        content: 'teste nota 1',
+        date: '25/07/2020',
+        hour: '10:00',
       },
       {
-        title: 'notes',
-        content: 'teste',
+        title: 'nota 2',
+        content: 'teste nota 2',
+        date: '26/07/2020',
+        hour: '11:00',
       },
     ],
   })
